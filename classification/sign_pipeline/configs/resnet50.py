cb7933c800faf58e64c96cc6cd64e8d5ea0d2197
p.ostyakov <p.ostyakov@samsung.com>
2019-07-01 22:40:21 +0300

    Fix

--- classification/sign_pipeline/configs/resnet50.py
+++ classification/sign_pipeline/configs/resnet50.py
@@ -3,12 +3,12 @@ import random
 import numpy as np
 import torch
 import torch.nn as nn
-from torchvision.models.resnet import resnet34
+from torchvision.models.resnet import resnet50
 from torchvision.transforms import ToTensor, Normalize, Compose, RandomAffine
 
 from .base import ConfigSignBase, PredictConfigSignBase, NUM_CLASSES
 
-MODEL_SAVE_PATH = "models/sign_resnet34"
+MODEL_SAVE_PATH = "models/sign_resnet50"
 BATCH_SIZE = 128
 
 SEED = 85
@@ -18,7 +18,7 @@ torch.random.manual_seed(SEED)
 
 
 def get_model():
-    model = resnet34(pretrained=True)
+    model = resnet50(pretrained=True)
     model.fc = nn.Linear(model.fc.in_features, NUM_CLASSES)
     return model
 
